d9ae321900af3b3751aa3c3f5a843d7be1d9dd40
Joaquin Bondulich <bondulich12@gmail.com>
2024-03-22 13:40:47 +0100

    ajustes en componente heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,28 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../interfaces/hero';
-import { NgFor, UpperCasePipe } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { AsyncPipe, NgIf, TitleCasePipe } from '@angular/common';
 import { HeroService } from '../services/hero.service';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from '../components/dialog/dialog.component';
+import { RouterModule } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { SearchHeroComponent } from '../components/search-hero/search-hero.component';
+import { LoaderService } from '../services/loader.service';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe, NgFor, FormsModule],
+  imports: [SearchHeroComponent, MatListModule, MatIconModule, MatButtonModule, MatDividerModule, TitleCasePipe, RouterModule, AsyncPipe, NgIf],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent implements OnInit {
-  public heroes:Hero[] = [];
+  public heroes$ = new BehaviorSubject<Hero[]>([]);
 
-  constructor(private heroService: HeroService){ }
+  constructor(private heroService: HeroService, private dialog: MatDialog, private loaderService: LoaderService){ }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
+  onSearch(term: string){
+    this.heroService.searchHeroes(term).subscribe(res => this.heroes$.next(res));
+  }
+
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(res => {
-      this.heroes = res;
-    })
+    this.loaderService.showLoader();
+
+    this.heroService.getHeroes().subscribe({
+      next: res => this.heroes$.next(res),
+      complete: () => this.loaderService.hideLoader()
+    });
+  }
+
+  deleteHero(hero: Hero){
+    const dialogRef = this.dialog.open(DialogComponent, {data: hero, width: '250px'});
+
+    dialogRef.afterClosed().subscribe(accept => {
+      if(accept){
+        this.heroes$.next(this.heroes$.value.filter(h => h !== hero))
+        this.heroService.deleteHero(hero.id).subscribe();
+      }
+    });
   }
 }
