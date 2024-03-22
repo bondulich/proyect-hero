e7aeb69686d6a5e8dc4381af51f847158fd7a9e1
Joaquin Bondulich <bondulich12@gmail.com>
2024-03-22 04:15:35 +0100

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'heroes', pathMatch: 'full' },
+    { path: 'heroes', component: HeroesComponent },
+    { path: 'hero-detail', loadComponent: () => import('./hero-detail/hero-detail.component').then( m => m.HeroDetailComponent) },
+    { path: 'hero-detail/:id', loadComponent: () => import('./hero-detail/hero-detail.component').then( m => m.HeroDetailComponent) },
+    { path: '**', redirectTo: 'heroes'}
+];
